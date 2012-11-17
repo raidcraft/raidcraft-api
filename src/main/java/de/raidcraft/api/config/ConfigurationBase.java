ad91b422679efa0756cabc437ec251254775e432
Silthus <silthus@raid-craft.de>
2012-11-17 18:02:41 +0100

    next try - changing around the order of the file loading and saving

--- src/main/java/de/raidcraft/api/config/ConfigurationBase.java
+++ src/main/java/de/raidcraft/api/config/ConfigurationBase.java
@@ -1,19 +1,15 @@
 package de.raidcraft.api.config;
 
 import com.sk89q.rebar.config.ConfigurationException;
-import com.sk89q.rebar.config.ConfigurationNode;
 import com.sk89q.rebar.config.YamlConfigurationFile;
 import com.sk89q.rebar.config.YamlStyle;
 import com.sk89q.rebar.config.annotations.Configurator;
-import com.sk89q.rebar.config.annotations.Setting;
 import com.sk89q.rebar.util.DefaultsUtils;
-import de.raidcraft.RaidCraft;
 import de.raidcraft.api.BasePlugin;
 import org.yaml.snakeyaml.DumperOptions;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
 
 /**
  * @author Silthus
@@ -34,53 +30,23 @@ public class ConfigurationBase extends YamlConfigurationFile {
 
         try {
             // lets create some defaults if the file does not exist
-            Configurator configurator = new SimpleConfigurator();
+            Configurator configurator = new Configurator();
             configurator.registerInstance(this);
             // create the normal defaults file if it does not exist
             if (!file.exists()) {
                 DefaultsUtils.createDefaultConfiguration(this.getClass(), file, "defaults/" + name);
-                load();
-                configurator.load(this, this);
                 save();
-            } else {
-                // it is important to load the config first or else it wont use the @Setting annotations
-                load();
-                configurator.load(this, this);
             }
+            // it is important to load the config first or else it wont use the @Setting annotations
+            load();
+            configurator.load(this, this);
+            save();
         } catch (IOException e) {
             plugin.getLogger().warning(e.getMessage());
             e.printStackTrace();
         } catch (ConfigurationException e) {
             plugin.getLogger().warning(e.getMessage());
             e.printStackTrace();
-        }
-    }
-
-    public class SimpleConfigurator extends Configurator {
-
-        @Override
-        protected boolean handleLoad(Field field, Object object, Setting setting, Object value) throws ConfigurationException {
-
-            String path = getSettingPath(field, setting);
-            try {
-                if (get(path) != null) {
-                    value = get(path);
-                }
-                field.setAccessible(true);
-                field.set(object, value);
-            } catch (IllegalAccessException e) {
-                RaidCraft.LOGGER.warning(e.getMessage());
-                e.printStackTrace();
-                return true;
-            }
-            return true;
-        }
-
-        @Override
-        protected boolean handleSave(ConfigurationNode node, String path, Setting setting, Object value) {
-
-            set(path, value);
-            return false;
         }
     }
 }
