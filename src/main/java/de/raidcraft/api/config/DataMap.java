0b8dfab98f72c3f2c2d76d7674be2752c0ca5647
Silthus <silthus@raid-craft.de>
2012-12-20 21:24:28 +0100

    optimize imports

--- src/main/java/de/raidcraft/api/config/DataMap.java
+++ src/main/java/de/raidcraft/api/config/DataMap.java
@@ -1,6 +1,8 @@
 package de.raidcraft.api.config;
 
-import org.bukkit.configuration.*;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemoryConfiguration;
 
 import java.util.Map;
 
