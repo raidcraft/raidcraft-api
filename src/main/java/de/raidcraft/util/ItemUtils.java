73132a1f5afacdb22d59ffb202861fa158d396df
Philip Urban <urban.philip@googlemail.com>
2012-12-30 21:25:15 +0100

    added some language handler for item names

--- src/main/java/de/raidcraft/util/ItemUtils.java
+++ src/main/java/de/raidcraft/util/ItemUtils.java
@@ -37,6 +37,85 @@ public final class ItemUtils {
     }
 
     public static String getFriendlyName(Material material) {
+
         return WordUtils.capitalize(material.name().replace("_", " ").toLowerCase());
+    }
+
+    public static String getFriendlyName(Material material, Language language) {
+
+        if(Item.getItemByMaterial(material) != null) {
+            return Item.getItemByMaterial(material).getFriendlyName(language);
+        }
+        return getFriendlyName(material);
+    }
+
+    public enum Item {
+
+        // tools
+        WOODEN_SHOVEL(Material.WOOD_SPADE, ItemType.TOOL, "Holzschaufel"),
+        STONE_SHOVEL(Material.STONE_SPADE, ItemType.TOOL, "Steinschaufel"),
+        IRON_SHOVEL(Material.IRON_SPADE, ItemType.TOOL, "Eisenschaufel"),
+        GOLD_SHOVEL(Material.GOLD_SPADE, ItemType.TOOL, "Goldschaufel"),
+        DIAMOND_SHOVEL(Material.DIAMOND_SPADE, ItemType.TOOL, "Diamantschaufel"),
+
+        WOODEN_PICKAXE(Material.WOOD_PICKAXE, ItemType.TOOL, "Holzspitzhacke"),
+        STONE_PICKAXE(Material.STONE_PICKAXE, ItemType.TOOL, "Steinspitzhacke"),
+        IRON_PICKAXE(Material.IRON_PICKAXE, ItemType.TOOL, "Eisenspitzhacke"),
+        GOLD_PICKAXE(Material.GOLD_PICKAXE, ItemType.TOOL, "Goldspitzhacke"),
+        DIAMOND_PICKAXE(Material.DIAMOND_PICKAXE, ItemType.TOOL, "Diamantspitzhacke");
+
+
+        private Material material;
+        private ItemType type;
+        private String germanFriendlyName;
+
+        private Item(Material material, ItemType type, String germanFriendlyName) {
+            this.material = material;
+            this.type = type;
+            this.germanFriendlyName = germanFriendlyName;
+        }
+
+        public Material getMaterial() {
+            return material;
+        }
+
+        public ItemType getType() {
+            return type;
+        }
+
+        public String getGermanFriendlyName() {
+            return germanFriendlyName;
+        }
+
+        public String getEnglishFriendlyName() {
+            return ItemUtils.getFriendlyName(material);
+        }
+
+        public String getFriendlyName(Language language) {
+
+            if(language == Language.GERMAN) {
+                return getGermanFriendlyName();
+            }
+
+            return getEnglishFriendlyName();
+        }
+
+        public static Item getItemByMaterial(Material material) {
+            for(Item item : Item.values()) {
+                if(item.getMaterial().equals(material)) {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+
+    public enum ItemType {
+        TOOL
+    }
+
+    public enum Language {
+        ENGLISH,
+        GERMAN
     }
 }
