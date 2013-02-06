b31b58be2bb45348fb469acb4167291509d31e22
Silthus <silthus@raid-craft.de>
2013-02-06 15:22:15 +0100

    fixed the queued command type insecurity

--- src/main/java/de/raidcraft/api/commands/QueuedCommand.java
+++ src/main/java/de/raidcraft/api/commands/QueuedCommand.java
@@ -2,6 +2,7 @@ package de.raidcraft.api.commands;
 
 import de.raidcraft.RaidCraft;
 import de.raidcraft.RaidCraftPlugin;
+import org.apache.commons.lang.ClassUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
@@ -15,10 +16,10 @@ public class QueuedCommand {
 
     private final CommandSender sender;
     private final Object object;
-    private final Method method;
     private final Object[] args;
+    private Method method;
 
-    public QueuedCommand(final CommandSender sender, Object object, String method, Object... args) throws NoSuchMethodException {
+    public QueuedCommand(final CommandSender sender, Object object, String methodName, Object... args) throws NoSuchMethodException {
 
         this.sender = sender;
         this.object = object;
@@ -26,9 +27,35 @@ public class QueuedCommand {
         for (int i = 0; i < args.length; i++) {
             argsClasses[i] = args[i].getClass();
         }
-        this.method = object.getClass().getDeclaredMethod(method, argsClasses);
+        for (Method method : object.getClass().getDeclaredMethods()) {
+            if (method.getParameterTypes().length == argsClasses.length) {
+                boolean match = true;
+                for (int i = 0; i < argsClasses.length; i++) {
+                    if (method.getParameterTypes()[i].isPrimitive()) {
+                        Class<?> aClass = ClassUtils.primitiveToWrapper(method.getParameterTypes()[i]);
+                        if (aClass.isAssignableFrom(argsClasses[i])) {
+                            continue;
+                        } else {
+                            match = false;
+                            break;
+                        }
+                    }
+                    if (!method.getParameterTypes()[i].isAssignableFrom(argsClasses[i])) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    this.method = method;
+                    break;
+                }
+            }
+        }
+        if (this.method == null) throw new NoSuchMethodException(
+                "No method signature found for " + methodName + " in " + object.getClass().getCanonicalName());
         this.args = args;
         RaidCraft.getComponent(RaidCraftPlugin.class).queueCommand(this);
+        sender.sendMessage(ChatColor.RED + "Bitte bestätige den Befehl mit: " + ChatColor.GREEN + "/rcconfirm");
     }
 
     public CommandSender getSender() {
