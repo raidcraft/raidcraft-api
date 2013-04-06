32d109c80acd58c7a1b66e0d4882d4639ae8ecda
Silthus <silthus@raid-craft.de>
2013-04-06 16:41:04 +0200

    moved inner class out of the rc plugin class

---
+++ src/main/java/de/raidcraft/PlayerPlacedBlock.java
@@ -0,0 +1,107 @@
+package de.raidcraft;
+
+import de.raidcraft.api.database.Bean;
+import org.bukkit.block.Block;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+* @author Silthus
+*/
+@Entity
+@Table(name = "rc_player_placed_blocks")
+public class PlayerPlacedBlock implements Bean {
+
+    @Id
+    private int id;
+    private String world;
+    private int x;
+    private int y;
+    private int z;
+
+    public PlayerPlacedBlock(Block block) {
+
+        this(block.getWorld().getName(), block.getX(), block.getY(), block.getZ());
+    }
+
+    public PlayerPlacedBlock(String world, int x, int y, int z) {
+
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    public String getWorld() {
+
+        return world;
+    }
+
+    public void setWorld(String world) {
+
+        this.world = world;
+    }
+
+    public int getX() {
+
+        return x;
+    }
+
+    public void setX(int x) {
+
+        this.x = x;
+    }
+
+    public int getY() {
+
+        return y;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
+
+    public int getZ() {
+
+        return z;
+    }
+
+    public void setZ(int z) {
+
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlayerPlacedBlock that = (PlayerPlacedBlock) o;
+
+        return x == that.x && y == that.y && z == that.z && world.equals(that.world);
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = world.hashCode();
+        result = 31 * result + x;
+        result = 31 * result + y;
+        result = 31 * result + z;
+        return result;
+    }
+}
--- src/main/java/de/raidcraft/RaidCraftPlugin.java
+++ src/main/java/de/raidcraft/RaidCraftPlugin.java
@@ -6,13 +6,9 @@ import de.raidcraft.api.Component;
 import de.raidcraft.api.commands.ConfirmCommand;
 import de.raidcraft.api.config.ConfigurationBase;
 import de.raidcraft.api.config.Setting;
-import de.raidcraft.api.database.Bean;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -112,99 +108,4 @@ public class RaidCraftPlugin extends BasePlugin implements Component {
         Ebean.delete(remove);
     }
 
-    @Entity
-    @Table(name = "rc_player_placed_blocks")
-    public static class PlayerPlacedBlock implements Bean {
-
-        @Id
-        private int id;
-        private String world;
-        private int x;
-        private int y;
-        private int z;
-
-        public PlayerPlacedBlock(Block block) {
-
-            this(block.getWorld().getName(), block.getX(), block.getY(), block.getZ());
-        }
-
-        public PlayerPlacedBlock(String world, int x, int y, int z) {
-
-            this.world = world;
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        public int getId() {
-
-            return id;
-        }
-
-        public void setId(int id) {
-
-            this.id = id;
-        }
-
-        public String getWorld() {
-
-            return world;
-        }
-
-        public void setWorld(String world) {
-
-            this.world = world;
-        }
-
-        public int getX() {
-
-            return x;
-        }
-
-        public void setX(int x) {
-
-            this.x = x;
-        }
-
-        public int getY() {
-
-            return y;
-        }
-
-        public void setY(int y) {
-
-            this.y = y;
-        }
-
-        public int getZ() {
-
-            return z;
-        }
-
-        public void setZ(int z) {
-
-            this.z = z;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            PlayerPlacedBlock that = (PlayerPlacedBlock) o;
-
-            return x == that.x && y == that.y && z == that.z && world.equals(that.world);
-        }
-
-        @Override
-        public int hashCode() {
-
-            int result = world.hashCode();
-            result = 31 * result + x;
-            result = 31 * result + y;
-            result = 31 * result + z;
-            return result;
-        }
-    }
 }
