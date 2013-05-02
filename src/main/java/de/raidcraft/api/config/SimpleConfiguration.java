d7f2c3a49af3b1f0529b4517a5b729974a1f9f03
Silthus <silthus@raid-craft.de>
2013-05-02 20:56:12 +0200

    added file option to simple config

--- src/main/java/de/raidcraft/api/config/SimpleConfiguration.java
+++ src/main/java/de/raidcraft/api/config/SimpleConfiguration.java
@@ -2,13 +2,20 @@ package de.raidcraft.api.config;
 
 import de.raidcraft.api.BasePlugin;
 
+import java.io.File;
+
 /**
  * @author Silthus
  */
-public class SimpleConfiguration extends ConfigurationBase {
+public class SimpleConfiguration<T extends BasePlugin> extends ConfigurationBase<T> {
 
-    public SimpleConfiguration(BasePlugin plugin, String name) {
+    public SimpleConfiguration(T plugin, String name) {
 
         super(plugin, name);
+    }
+
+    public SimpleConfiguration(T plugin, File file) {
+
+        super(plugin, file);
     }
 }
