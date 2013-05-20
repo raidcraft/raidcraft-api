d943f579c146005fe23a2b1ea03d68638003f4cc
Silthus <silthus@raid-craft.de>
2013-05-20 12:59:24 +0200

    fixed rebuild method

--- src/main/java/de/raidcraft/api/items/CustomItem.java
+++ src/main/java/de/raidcraft/api/items/CustomItem.java
@@ -29,7 +29,7 @@ public interface CustomItem extends RequirementResolver<Player> {
 
     public boolean matches(ItemStack itemStack);
 
-    public boolean rebuild(ItemStack itemStack);
+    public void rebuild(ItemStack itemStack);
 
     public CustomItemStack createNewItem();
 }
