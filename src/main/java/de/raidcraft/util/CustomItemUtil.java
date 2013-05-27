17627fa55bf3b87e53d6061422022eb7d2de9a76
Philip Urban <urban.philip@googlemail.com>
2013-05-27 23:38:43 +0200

    improved price string

--- src/main/java/de/raidcraft/util/CustomItemUtil.java
+++ src/main/java/de/raidcraft/util/CustomItemUtil.java
@@ -97,13 +97,18 @@ public final class CustomItemUtil {
 
     public static String getSellPriceString(double price) {
 
+        ChatColor priceColor = ChatColor.WHITE;
+        if(price < 0) {
+            priceColor = ChatColor.RED;
+        }
+        price = Math.abs(price);
         StringBuilder sb = new StringBuilder();
         int gold = (int) price / 100;
-        sb.append(ChatColor.WHITE).append(gold).append(ChatColor.GOLD).append("●");
+        sb.append(priceColor).append(gold).append(ChatColor.GOLD).append("●");
         int silver = (int) price % 100;
-        sb.append(ChatColor.WHITE).append(silver).append(ChatColor.GRAY).append("●");
+        sb.append(priceColor).append(silver).append(ChatColor.GRAY).append("●");
         int copper = (int) (price * 100) % 100;
-        sb.append(ChatColor.WHITE).append(copper).append(ChatColor.RED).append("●");
+        sb.append(priceColor).append(copper).append(ChatColor.RED).append("●");
         return sb.toString();
     }
 
