8e55f34cebbb3d78e2c3651e91ca4922b3572afa
Silthus <silthus@raid-craft.de>
2013-06-02 14:07:02 +0200

    added curse skill & effect

--- src/main/java/de/raidcraft/util/MathUtil.java
+++ src/main/java/de/raidcraft/util/MathUtil.java
@@ -8,4 +8,9 @@ import java.util.Random;
 public final class MathUtil {
 
     public static final Random RANDOM = new Random();
+
+    public static double toPercent(double percent) {
+
+        return ((int)((percent * 100.0) * 100)) / 100.0;
+    }
 }
