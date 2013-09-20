866f2ca1cc0fa335073bc1cf24ddbf5650bcde0b
Silthus <silthus@raid-craft.de>
2013-09-20 18:38:12 +0200

    tweaked the mobs and creatures

--- src/main/java/de/raidcraft/util/EntityUtil.java
+++ src/main/java/de/raidcraft/util/EntityUtil.java
@@ -1,11 +1,16 @@
 package de.raidcraft.util;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 
 /**
  * @author Silthus
  */
 public class EntityUtil {
+
+    private static final char HEALTH_BAR_MAIN_SYMBOL = '█';
+    private static final char HEALTH_BAR_HALF_SYMBOL = '▌';
+    private static final int HEALTH_BAR_LENGTH = 5;
 
      public static void addPanicMode(LivingEntity entity) {
 
@@ -24,5 +29,42 @@ public class EntityUtil {
             }
         }
         */
+    }
+
+    public static String drawHealthBar(double health, double maxHealth) {
+
+        ChatColor barColor = ChatColor.GREEN;
+        double healthInPercent = health / maxHealth;
+        if (healthInPercent < 0.20) {
+            barColor = ChatColor.DARK_RED;
+        } else if (healthInPercent < 0.35) {
+            barColor = ChatColor.RED;
+        } else if (healthInPercent < 0.50) {
+            barColor = ChatColor.GOLD;
+        } else if (healthInPercent < 0.75) {
+            barColor = ChatColor.YELLOW;
+        } else if (healthInPercent < 0.90) {
+            barColor = ChatColor.DARK_GREEN;
+        }
+
+        StringBuilder healthBar = new StringBuilder(ChatColor.BLACK + "[" + barColor + health + ChatColor.BLACK
+                + "/" + ChatColor.GREEN + maxHealth + ChatColor.BLACK + "]").append(barColor);
+
+        int count = (int) (healthInPercent * HEALTH_BAR_LENGTH);
+        double modulo = (healthInPercent * (HEALTH_BAR_LENGTH * 10)) % 10;
+        boolean appendHalfBar = modulo < 6 && modulo > 0;
+        for (int i = 0; i < count; i++) {
+            if (i == count - 1 && appendHalfBar) {
+                break;
+            }
+            healthBar.append(HEALTH_BAR_MAIN_SYMBOL);
+        }
+        if (appendHalfBar) {
+            healthBar.append(HEALTH_BAR_HALF_SYMBOL);
+        }
+        for (int i = 0; i < HEALTH_BAR_LENGTH - count; i++) {
+            healthBar.append("  ");
+        }
+        return healthBar.toString();
     }
 }
