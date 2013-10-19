a2bf171434643fe631fccda4a6b0ce3d8ac0e83b
Silthus <silthus@raid-craft.de>
2013-10-19 18:14:24 +0200

    player ambient thunder sound at the thunder location

--- src/main/java/de/raidcraft/util/EffectUtil.java
+++ src/main/java/de/raidcraft/util/EffectUtil.java
@@ -106,7 +106,7 @@ public class EffectUtil {
             if (LocationUtil.isWithinRadius(player.getLocation(), location, radius)) {
                 try {
                     ProtocolLibrary.getProtocolManager().sendServerPacket(player, lightning.getHandle());
-                    player.playSound(player.getLocation(), Sound.AMBIENCE_THUNDER, 10, 1);
+                    player.playSound(location, Sound.AMBIENCE_THUNDER, 10, 1);
                 } catch (InvocationTargetException e) {
                     RaidCraft.LOGGER.warning(e.getMessage());
                 }
