6b691ec2ac3c348192942ca2755c5f887b399d41
Silthus <silthus@raid-craft.de>
2014-03-01 15:56:53 +0100

    added message option to translator

--- src/main/java/de/raidcraft/api/language/Translator.java
+++ src/main/java/de/raidcraft/api/language/Translator.java
@@ -2,6 +2,7 @@ package de.raidcraft.api.language;
 
 import de.raidcraft.RaidCraft;
 import de.raidcraft.api.BasePlugin;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 /**
@@ -31,5 +32,29 @@ public class Translator {
 
         BasePlugin plugin = RaidCraft.getComponent(clazz);
         return plugin.getTranslationProvider().tr(player, key, def, args);
+    }
+
+    public static void msg(Class<? extends BasePlugin> clazz, CommandSender sender, String key, Object... args) {
+
+        BasePlugin plugin = RaidCraft.getComponent(clazz);
+        plugin.getTranslationProvider().msg(sender, key, args);
+    }
+
+    public static void msg(Class<? extends BasePlugin> clazz, CommandSender sender, String key, String def, Object... args) {
+
+        BasePlugin plugin = RaidCraft.getComponent(clazz);
+        plugin.getTranslationProvider().msg(sender, key, def, args);
+    }
+
+    public static void msg(Class<? extends BasePlugin> clazz, Player player, String key, Object... args) {
+
+        BasePlugin plugin = RaidCraft.getComponent(clazz);
+        plugin.getTranslationProvider().msg(player, key, args);
+    }
+
+    public static void msg(Class<? extends BasePlugin> clazz, Player player, String key, String def, Object... args) {
+
+        BasePlugin plugin = RaidCraft.getComponent(clazz);
+        plugin.getTranslationProvider().msg(player, key, def, args);
     }
 }
