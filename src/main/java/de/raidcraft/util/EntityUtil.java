6957b8c10d84b5dff32ebd952dd3fc3b2aa57b0f
Silthus <silthus@raid-craft.de>
2014-03-25 20:20:59 +0100

    fixed NMS compilation for 1.7.5

--- src/main/java/de/raidcraft/util/EntityUtil.java
+++ src/main/java/de/raidcraft/util/EntityUtil.java
@@ -1,14 +1,14 @@
 package de.raidcraft.util;
 
-import net.minecraft.server.v1_7_R1.EntityCreature;
-import net.minecraft.server.v1_7_R1.EntityInsentient;
-import net.minecraft.server.v1_7_R1.EntityLiving;
-import net.minecraft.server.v1_7_R1.PathfinderGoalPanic;
-import net.minecraft.server.v1_7_R1.PathfinderGoalSelector;
+import net.minecraft.server.v1_7_R2.EntityCreature;
+import net.minecraft.server.v1_7_R2.EntityInsentient;
+import net.minecraft.server.v1_7_R2.EntityLiving;
+import net.minecraft.server.v1_7_R2.PathfinderGoalPanic;
+import net.minecraft.server.v1_7_R2.PathfinderGoalSelector;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_7_R1.entity.CraftCreature;
-import org.bukkit.craftbukkit.v1_7_R1.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_7_R2.entity.CraftCreature;
+import org.bukkit.craftbukkit.v1_7_R2.entity.CraftLivingEntity;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.LivingEntity;
 
