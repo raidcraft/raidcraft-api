9fed403ab922a9d3cb57d508aa33d1c6de530635
Philip Urban <urban.philip@googlemail.com>
2014-04-23 21:48:39 +0200

    hopefully fixed trigger registration

--- src/main/java/de/raidcraft/api/quests/Quests.java
+++ src/main/java/de/raidcraft/api/quests/Quests.java
@@ -7,6 +7,7 @@ import de.raidcraft.util.CaseInsensitiveMap;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -63,19 +64,19 @@ public class Quests {
             }
         }
         queuedConfigLoader.clear();
-
-//        for(QuestTrigger trigger : loadedTrigger) {
-//            if(trigger instanceof Listener) {
-//                HandlerList.unregisterAll((Listener) trigger);
-//            }
-//        }
-//        questTrigger.clear();
-//        loadedTrigger.clear();
     }
 
     public static void disable(QuestProvider questProvider) {
 
         provider = null;
+
+        for(QuestTrigger trigger : loadedTrigger) {
+            if(trigger instanceof Listener) {
+                HandlerList.unregisterAll((Listener) trigger);
+            }
+        }
+        questTrigger.clear();
+        loadedTrigger.clear();
     }
 
     public static boolean isEnabled() {
