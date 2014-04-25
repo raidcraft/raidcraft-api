47765374ee46fba48cfed93fbeec72b6e70472b9
Philip Urban <urban.philip@googlemail.com>
2014-04-25 19:11:55 +0200

    removed debug msgs

--- src/main/java/de/raidcraft/api/quests/QuestTrigger.java
+++ src/main/java/de/raidcraft/api/quests/QuestTrigger.java
@@ -1,6 +1,5 @@
 package de.raidcraft.api.quests;
 
-import de.raidcraft.RaidCraft;
 import de.raidcraft.api.quests.quest.trigger.Trigger;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
@@ -53,7 +52,6 @@ public abstract class QuestTrigger {
     protected final void inform(String action, Player player) {
 
         if (getName().endsWith(action)) {
-            RaidCraft.LOGGER.info("[Quest] Quest trigger: '" + action + "' by player: " + player.getName());
             trigger.trigger(Quests.getQuestHolder(player));
         }
     }
