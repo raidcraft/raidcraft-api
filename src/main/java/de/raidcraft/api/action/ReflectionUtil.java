e571827d7516c6ec2a0c11dd1fbc733ea8be851c
Silthus <silthus@raid-craft.de>
2014-05-04 18:10:12 +0200

    added debug message

--- src/main/java/de/raidcraft/api/action/ReflectionUtil.java
+++ src/main/java/de/raidcraft/api/action/ReflectionUtil.java
@@ -1,5 +1,7 @@
 package de.raidcraft.api.action;
 
+import de.raidcraft.RaidCraft;
+
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 
@@ -11,6 +13,7 @@ public class ReflectionUtil {
     public static boolean isMatchingGenericMethodType(Method method, Class<?> matchingType) {
 
         for (Type type : method.getGenericParameterTypes()) {
+            RaidCraft.LOGGER.info("matching " + type.getTypeName() + " against " + matchingType.getTypeName());
             if (type.getClass().isAssignableFrom(matchingType)) return true;
         }
         return false;
