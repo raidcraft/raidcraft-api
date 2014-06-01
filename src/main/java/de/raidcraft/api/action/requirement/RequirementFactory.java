ea681270c4676e2f300e776ff5647954dc58d4a3
Silthus <md.doering@yahoo.de>
2014-06-01 09:38:19 +0200

    added player location requirement

--- src/main/java/de/raidcraft/api/action/requirement/RequirementFactory.java
+++ src/main/java/de/raidcraft/api/action/requirement/RequirementFactory.java
@@ -3,8 +3,12 @@ package de.raidcraft.api.action.requirement;
 import de.raidcraft.RaidCraft;
 import de.raidcraft.api.Component;
 import de.raidcraft.util.CaseInsensitiveMap;
+import de.raidcraft.util.LocationUtil;
 import lombok.NonNull;
 import lombok.SneakyThrows;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -35,6 +39,32 @@ public final class RequirementFactory implements Component {
     private void registerGlobalRequirements() {
 
         registerRequirement("player.is-sprinting", Player::isSprinting);
+        registerRequirement("player.location", new Requirement<Player>() {
+            @Override
+            public boolean test(Player player) {
+
+                Location location = player.getLocation();
+                ConfigurationSection config = getConfig();
+                World world = Bukkit.getWorld(config.getString("world"));
+                if (world == null) return false;
+
+                if (config.isSet("x") && config.isSet("y") && config.isSet("z") && config.isSet("world")) {
+                    if (config.isSet("radius")) {
+                        return LocationUtil.isWithinRadius(location,
+                                new Location(world,
+                                        config.getInt("x"),
+                                        config.getInt("y"),
+                                        config.getInt("z")),
+                                config.getInt("radius"));
+                    }
+                    return config.getInt("x") == location.getBlockX()
+                            && config.getInt("y") == location.getBlockY()
+                            && config.getInt("z") == location.getBlockZ()
+                            && world.equals(location.getWorld());
+                }
+                return false;
+            }
+        });
     }
 
     private <T> void registerRequirement(@NonNull String identifier, @NonNull Requirement<T> requirement) {
