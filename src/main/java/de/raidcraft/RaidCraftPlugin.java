594e7459af00b3f9110b46ffbc6cd7eeb9f2864d
Denis Cundari <denis@cundari.de>
2014-07-05 02:55:11 +0200

    added entity bean for multi language support

--- src/main/java/de/raidcraft/RaidCraftPlugin.java
+++ src/main/java/de/raidcraft/RaidCraftPlugin.java
@@ -14,6 +14,7 @@ import de.raidcraft.api.inventory.TPersistentInventory;
 import de.raidcraft.api.inventory.TPersistentInventorySlot;
 import de.raidcraft.api.items.CustomItemManager;
 import de.raidcraft.api.items.attachments.ItemAttachmentManager;
+import de.raidcraft.api.language.table.TLanguage;
 import de.raidcraft.api.storage.TObjectStorage;
 import de.raidcraft.util.TimeUtil;
 import de.raidcraft.util.bossbar.BarAPI;
@@ -112,6 +113,7 @@ public class RaidCraftPlugin extends BasePlugin implements Component, Listener {
         classes.add(TObjectStorage.class);
         classes.add(TPersistentInventory.class);
         classes.add(TPersistentInventorySlot.class);
+        classes.add(TLanguage.class);
         return classes;
     }
 
