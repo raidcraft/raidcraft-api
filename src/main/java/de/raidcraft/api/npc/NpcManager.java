20bf9ba00c400fa2d5ee6eb76ebda6df6b49cd09
Dragonfire <bastian616-git@yahoo.de>
2014-07-15 23:07:21 +0200

    start npc rework

---
+++ src/main/java/de/raidcraft/api/npc/NpcManager.java
@@ -0,0 +1,11 @@
+package de.raidcraft.api.npc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sebastian
+ * Date: 15.07.14
+ * Time: 23:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class NpcManager {
+}
