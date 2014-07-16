abe5e07eb3f6323e3c82251d7bcc775894fd7110
Dragonfire <bastian616-git@yahoo.de>
2014-07-17 01:04:57 +0200

    add global trait name settings for all traits

---
+++ src/main/java/de/raidcraft/api/npc/RC_Traits.java
@@ -0,0 +1,14 @@
+package de.raidcraft.api.npc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sebastian
+ * Date: 17.07.14
+ * Time: 01:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class RC_Traits {
+    public static String CONSERVATION = "rcconversations";
+    public static String QUEST = "quest";
+    public static String STATION = "station";
+}
