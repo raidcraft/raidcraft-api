e88ff037fd5493e8a386910ba4aecfd1e859a15a
Silthus <silthus@raid-craft.de>
2014-07-17 20:24:37 +0200

    forgot to register req mapping class

--- src/main/java/de/raidcraft/RaidCraftPlugin.java
+++ src/main/java/de/raidcraft/RaidCraftPlugin.java
@@ -5,6 +5,8 @@ import de.raidcraft.api.Component;
 import de.raidcraft.api.action.ActionCommand;
 import de.raidcraft.api.action.action.ActionFactory;
 import de.raidcraft.api.action.requirement.RequirementFactory;
+import de.raidcraft.api.action.requirement.tables.TPersistantRequirement;
+import de.raidcraft.api.action.requirement.tables.TPersistantRequirementMapping;
 import de.raidcraft.api.action.trigger.TriggerManager;
 import de.raidcraft.api.commands.ConfirmCommand;
 import de.raidcraft.api.config.ConfigurationBase;
@@ -115,6 +117,7 @@ public class RaidCraftPlugin extends BasePlugin implements Component, Listener {
         classes.add(TPersistentInventory.class);
         classes.add(TPersistentInventorySlot.class);
         classes.add(TPersistantRequirement.class);
+        classes.add(TPersistantRequirementMapping.class);
         classes.add(TLanguage.class);
         return classes;
     }
