a9e00fde5d5ba88f64d8e78b4133799e86999f7e
Dragonfire <bastian616-git@yahoo.de>
2014-07-17 18:09:10 +0200

    comment Translator

--- src/main/java/de/raidcraft/api/language/Translator.java
+++ src/main/java/de/raidcraft/api/language/Translator.java
@@ -6,6 +6,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 /**
+ * for params in String:
+ * http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html
+ * alternativ myplugin.getTranslationProvider().tr(...)
  * @author Silthus
  */
 public class Translator {
