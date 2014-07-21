d3abb4eefaedca3c03ac2fad662aee664a0ee078
Dragonfire <bastian616-git@yahoo.de>
2014-07-21 13:27:27 +0200

    add graveyards trait name

--- src/main/java/de/raidcraft/api/npc/RC_Traits.java
+++ src/main/java/de/raidcraft/api/npc/RC_Traits.java
@@ -12,4 +12,5 @@ public class RC_Traits {
     public static String QUEST = "quest";
     public static String STATION = "station";
     public static String TALK_CLOSE = "talkclose";
+    public static String GRAVEYARDS = "rcgraveyards";
 }
