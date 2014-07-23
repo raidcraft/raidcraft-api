86132268316f325fab0532a458c3f36960d83ca1
Denis Cundari <denis@cundari.de>
2014-07-23 20:31:36 +0200

    Added #1154 reference to chat colors and their meaning

---
+++ src/main/java/de/raidcraft/reference/Colors.java
@@ -0,0 +1,18 @@
+package de.raidcraft.reference;
+
+import org.bukkit.ChatColor;
+
+/**
+ * 
+ */
+public final class Colors {
+
+    public static final class Chat {
+
+        public static final ChatColor INFO = ChatColor.YELLOW;
+        public static final ChatColor SUCCESS = ChatColor.GREEN;
+        public static final ChatColor ERROR = ChatColor.RED;
+        public static final ChatColor WARNING = ChatColor.DARK_RED;
+        public static final ChatColor SPECIAL = ChatColor.GOLD;
+    }
+}
