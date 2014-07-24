bec17bb2e8f5a2415624929cf7cfae4698711233
Dragonfire <bastian616-git@yahoo.de>
2014-07-24 12:00:16 +0200

    add useful variables for Inventory

---
+++ src/main/java/de/raidcraft/api/inventory/RcInventory.java
@@ -0,0 +1,9 @@
+package de.raidcraft.api.inventory;
+
+/**
+ * @author Dragonfire
+ */
+public class RcInventory {
+    public final static int COLUMN_COUNT = 9;
+    public final static int MAX_ROWS = 6;
+}
