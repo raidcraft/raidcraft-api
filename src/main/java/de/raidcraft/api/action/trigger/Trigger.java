6fbfca5776696252cb84a5ac83047c813bbf97f1
Silthus <silthus@raid-craft.de>
2014-07-25 19:18:56 +0200

    debugging fixes

--- src/main/java/de/raidcraft/api/action/trigger/Trigger.java
+++ src/main/java/de/raidcraft/api/action/trigger/Trigger.java
@@ -1,6 +1,5 @@
 package de.raidcraft.api.action.trigger;
 
-import com.sk89q.util.StringUtil;
 import de.raidcraft.RaidCraft;
 import de.raidcraft.util.CaseInsensitiveMap;
 import lombok.Data;
@@ -55,8 +54,8 @@ public abstract class Trigger implements TriggerConfigGenerator {
     protected final <T> void informListeners(@NonNull String action, @NonNull T triggeringEntity, @NonNull Predicate<ConfigurationSection> predicate) {
 
         String identifier = getIdentifier() + "." + action;
-        RaidCraft.LOGGER.info("Informing listeners of " + identifier + ": " + StringUtil.joinString(registeredListeners.get(identifier), ",", 0));
         if (registeredListeners.containsKey(identifier)) {
+            RaidCraft.LOGGER.info("Informing listeners of " + identifier);
             new ArrayList<>(registeredListeners.get(identifier)).stream()
                     .map(wrapper -> (TriggerListenerConfigWrapper<T>) wrapper)
                     .filter(wrapper -> wrapper != null && wrapper.getTriggerListener() != null)
