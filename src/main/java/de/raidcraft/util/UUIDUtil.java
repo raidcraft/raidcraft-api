8721a4558f9b685eeabd1c26f7f4d8d0dd924b29
Dragonfire <bastian616-git@yahoo.de>
2014-07-30 01:52:56 +0200

    add more uuid stuff

--- src/main/java/de/raidcraft/util/UUIDUtil.java
+++ src/main/java/de/raidcraft/util/UUIDUtil.java
@@ -1,7 +1,9 @@
 package de.raidcraft.util;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
@@ -28,5 +30,19 @@ public class UUIDUtil {
             }
         }
         return Bukkit.getOfflinePlayer(name).getUniqueId();
+    }
+
+    public static String getUUIDStringFromName(String name) {
+        UUID uuid = convertPlayer(name);
+        return uuid != null ? uuid.toString() : null;
+    }
+
+    public static String getNameFromUUID(String uuid) {
+        return Bukkit.getOfflinePlayer(UUID.fromString(uuid)).getName();
+    }
+
+    public static String castUUID(CommandSender sender) {
+
+        return ((Player) sender).getUniqueId().toString();
     }
 }
