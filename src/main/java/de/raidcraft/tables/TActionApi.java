f8bb90321b94844a9c322e1426d473cfd2530a26
Dragonfire <bastian616-git@yahoo.de>
2014-08-14 19:29:45 +0200

    change tabel structure refs #1360

--- src/main/java/de/raidcraft/tables/TActionApi.java
+++ src/main/java/de/raidcraft/tables/TActionApi.java
@@ -10,21 +10,16 @@ import javax.persistence.Table;
 /**
  * @author Dragonfire
  */
+@Getter
+@Setter
 @Entity
 @Table(name = "rc_actionapi")
 public class TActionApi {
 
-    @Getter
-    @Setter
+
     @Id
     private int id;
-    @Getter
-    @Setter
     private String action_type;
-    @Getter
-    @Setter
     private String name;
-    @Getter
-    @Setter
     private String description;
 }
