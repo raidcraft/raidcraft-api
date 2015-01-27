c1e7349baefb8a959993172a23fd518d78b89e9d
Dragonfire <bastian616-git@yahoo.de>
2015-01-28 00:53:05 +0100

    ChunkLocation MultiWorld Support fixed #1350

--- src/main/java/de/raidcraft/util/ChunkLocation.java
+++ src/main/java/de/raidcraft/util/ChunkLocation.java
@@ -1,75 +1,40 @@
 package de.raidcraft.util;
 
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
-import org.bukkit.World;
+
+import java.util.UUID;
 
 /**
- * @author Philip Urban
+ * @author Philip Urban, IDragonfire
  */
+@Getter
+@EqualsAndHashCode
 public class ChunkLocation {
 
     private int x;
     private int z;
+    private UUID world;
 
-    public ChunkLocation(int x, int z) {
-
+    public ChunkLocation(int x, int z, UUID world) {
         this.x = x;
         this.z = z;
+        this.world = world;
     }
 
     public ChunkLocation(Chunk chunk) {
-
-        this.x = chunk.getX();
-        this.z = chunk.getZ();
+        this(chunk.getX(), chunk.getZ(), chunk.getWorld().getUID());
     }
 
     public ChunkLocation(Location location) {
-
-        double cxd = location.getX() / 16D;
-        if (cxd < 0) {
-            x = (int) Math.ceil(cxd);
-        } else {
-            x = (int) cxd;
-        }
-
-        double czd = location.getZ() / 16D;
-        if (czd < 0) {
-            z = (int) Math.ceil(czd);
-        } else {
-            z = (int) czd;
-        }
+        this(location.getChunk());
     }
 
-    public int getX() {
 
-        return x;
-    }
-
-    public int getZ() {
-
-        return z;
-    }
-
-    public Chunk getChunk(World world) {
-
-        return world.getChunkAt(x, z);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return new HashCodeBuilder().append(x).append(z).toHashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-
-        if (obj instanceof ChunkLocation) {
-            ChunkLocation cl = (ChunkLocation) obj;
-            return (cl.getX() == x && cl.getZ() == z);
-        }
-        return false;
+    public Chunk getChunk() {
+        return Bukkit.getWorld(getWorld()).getChunkAt(getX(), getZ());
     }
 }
