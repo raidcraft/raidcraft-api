3ff716ee984b9cda8afbba95588c557dd3784183
Michael Döring <michael.doering@interhyp.de>
2015-04-27 16:24:23 +0200

    fixed compile errors from config creator refactor

--- src/main/java/de/raidcraft/api/action/action/global/DoorAction.java
+++ src/main/java/de/raidcraft/api/action/action/global/DoorAction.java
@@ -1,12 +1,6 @@
 package de.raidcraft.api.action.action.global;
 
-import com.sk89q.minecraft.util.commands.CommandContext;
-import de.raidcraft.api.BasePlugin;
-import de.raidcraft.api.action.ActionAPI;
 import de.raidcraft.api.action.action.Action;
-import de.raidcraft.api.config.builder.ConfigBuilder;
-import de.raidcraft.api.config.builder.ConfigBuilderException;
-import de.raidcraft.util.BlockUtil;
 import de.raidcraft.util.ConfigUtil;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
@@ -20,6 +14,15 @@ import org.bukkit.material.Openable;
 public class DoorAction implements Action<Player> {
 
     @Override
+    @Information(
+            value = "door.toggle",
+            desc = "Toggles (-t) the targeted door into an open (-o) or close state.",
+            conf = {
+                    "location: world,x,y,z coordinates of the door base",
+                    "toggle: false/true will toggle to the opposite state",
+                    "open: true/false will open/close the door"
+            }
+    )
     public void accept(Player player, ConfigurationSection config) {
 
         Location location = ConfigUtil.getLocationFromConfig(config, player);
@@ -31,31 +34,5 @@ public class DoorAction implements Action<Player> {
             state.setOpen(open);
             block.getState().update();
         }
-    }
-
-    @Information(
-            value = "door.toggle",
-            desc = "Toggles (-t) the targeted door into an open (-o) or close state.",
-            conf = {
-                    "location: world,x,y,z coordinates of the door base",
-                    "toggle: false/true will toggle to the opposite state",
-                    "open: true/false will open/close the door"
-            },
-            usage = "[-o/-t]",
-            flags = "ot",
-            help = "Target the openable block (door, trapdoor, gate, etc.) you want to toggle.",
-            multiSection = true
-    )
-    public <T extends BasePlugin> void build(ConfigBuilder<T> builder, CommandContext args, Player player) throws ConfigBuilderException {
-
-        ConfigurationSection config = createConfigSection();
-        Block block = BlockUtil.getTargetBlock(player, b -> b.getState().getData() instanceof Openable);
-        if (block == null) {
-            throw new ConfigBuilderException("No openable block (door, trapdoor, gate, etc.) found in sight!");
-        }
-        config.set("location", createLocationSection(block.getLocation()));
-        config.set("toggle", args.hasFlag('t'));
-        config.set("open", args.hasFlag('o'));
-        builder.append(this, config, getPath(), ActionAPI.getIdentifier(this));
     }
 }
