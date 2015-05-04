c8ed91fc1b9fd13c416748512e78727ed317caae
Michael Döring <michael.doering@interhyp.de>
2015-05-04 13:27:48 +0200

    added default implementation für resolve reason #1859

--- src/main/java/de/raidcraft/api/action/requirement/RequirementResolver.java
+++ src/main/java/de/raidcraft/api/action/requirement/RequirementResolver.java
@@ -1,5 +1,7 @@
 package de.raidcraft.api.action.requirement;
 
+import org.bukkit.entity.Player;
+
 import java.util.List;
 
 /**
@@ -9,7 +11,23 @@ public interface RequirementResolver<T> {
 
     public List<Requirement<T>> getRequirements();
 
-    public boolean isMeetingAllRequirements(T entity);
+    public default boolean isMeetingAllRequirements(T object) {
 
-    public String getResolveReason(T entity);
+        return getRequirements().stream().allMatch(requirement -> requirement.test(object));
+    }
+
+    @SuppressWarnings("unchecked")
+    public default String getResolveReason(T object) {
+
+        for (Requirement<T> requirement : getRequirements()) {
+            if (!requirement.test(object)) {
+                if (requirement instanceof Reasonable) {
+                    return ((Reasonable<T>) requirement).getReason(object);
+                } else {
+                    return "Reason not found!";
+                }
+            }
+        }
+        return "Alle Vorraussetzungen sind erfüllt.";
+    }
 }
