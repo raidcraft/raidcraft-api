a28e5811e42d4d630e35a61097911b2e50d8a46f
Michael Döring <michael.doering@interhyp.de>
2015-05-08 16:49:57 +0200

    added a pseudo random generator util class

---
+++ src/main/java/de/raidcraft/api/random/PseudoRandomGenerator.java
@@ -0,0 +1,29 @@
+package de.raidcraft.api.random;
+
+/**
+ * @author mdoering
+ */
+public class PseudoRandomGenerator {
+
+    private final double baseChance;
+    private int iteration = 1;
+
+    public PseudoRandomGenerator(double chance) {
+
+        if (chance > 1.0) chance = 2.0;
+        if (chance <= 0) chance = 1.0;
+        // lets calculate the base chance by taking the number of times the action should hit
+        // e.g. with a chance of 0.25 -> 25% 1/4 should be a hit
+        this.baseChance = chance / ((1.0 / chance) - 1);
+    }
+
+    public boolean isHit() {
+
+        if (Math.random() < (baseChance * iteration)) {
+            iteration = 1;
+            return true;
+        }
+        iteration++;
+        return false;
+    }
+}
