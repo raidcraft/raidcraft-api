93b669007ecdd0b7a36f8a83e1f680752f8387b9
Michael Döring <michael.doering@interhyp.de>
2015-06-23 16:04:24 +0200

    replace .conv with nothing to support more descriptive names

--- src/main/java/de/raidcraft/util/StringUtils.java
+++ src/main/java/de/raidcraft/util/StringUtils.java
@@ -7,6 +7,6 @@ public final class StringUtils {
 
     public static String formatName(String name) {
 
-        return name == null ? "" : name.toLowerCase().replace(" ", "-").trim();
+        return name == null ? "" : name.toLowerCase().replace(" ", "-").replace(".conv", "").trim();
     }
 }
