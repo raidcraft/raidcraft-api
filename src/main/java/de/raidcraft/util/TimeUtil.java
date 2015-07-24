4676aaaaf1d856de249f56b3498c39c16bac4682
Michael Döring <michael.doering@interhyp.de>
2015-07-24 15:48:06 +0200

    print accurate short formatted time

--- src/main/java/de/raidcraft/util/TimeUtil.java
+++ src/main/java/de/raidcraft/util/TimeUtil.java
@@ -1,6 +1,7 @@
 package de.raidcraft.util;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -85,6 +86,76 @@ public final class TimeUtil {
         } else {
             return (((int) (seconds * 100)) / 100.0) + "s";
         }
+    }
+
+    public static String getAccurrateFormatedTime(long millis) {
+
+        if(millis < 0) {
+            throw new IllegalArgumentException("Duration must be greater than zero!");
+        }
+
+        long days = TimeUnit.MILLISECONDS.toDays(millis);
+        millis -= TimeUnit.DAYS.toMillis(days);
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        millis -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+        StringBuilder sb = new StringBuilder(64);
+        if (days > 0) {
+            sb.append(days);
+            sb.append(days > 1 ? " Tage " : " Tag ");
+        }
+        if (hours > 0) {
+            sb.append(hours);
+            sb.append(hours > 1 ? " Stunden " : " Stunde ");
+        }
+        if (minutes > 0) {
+            sb.append(minutes);
+            sb.append(minutes > 1 ? " Minuten " : " Minute ");
+        }
+        if (seconds > 0) {
+            sb.append(seconds);
+            sb.append(seconds > 1 ? " Sekunden " : " Sekunde ");
+        }
+
+        return (sb.toString());
+    }
+
+    public static String getAccurrateShortFormatedTime(long millis) {
+
+        if (millis < 0) {
+            throw new IllegalArgumentException("Duration must be greater than zero!");
+        }
+
+        long days = TimeUnit.MILLISECONDS.toDays(millis);
+        millis -= TimeUnit.DAYS.toMillis(days);
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        millis -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+        StringBuilder sb = new StringBuilder(64);
+        if (days > 0) {
+            sb.append(days);
+            sb.append("d");
+        }
+        if (hours > 0) {
+            sb.append(hours);
+            sb.append("h");
+        }
+        if (minutes > 0) {
+            sb.append(minutes);
+            sb.append("min");
+        }
+        if (seconds > 0) {
+            sb.append(seconds);
+            sb.append("s");
+        }
+
+        return (sb.toString());
     }
 
     private static final Pattern TIME_PATTERN = Pattern.compile("^(([\\d]+)y)?(([\\d]+)d)?(([\\d]+)h)?(([\\d]+)m)?(([\\d]+)s)?([\\d]+)?$");
