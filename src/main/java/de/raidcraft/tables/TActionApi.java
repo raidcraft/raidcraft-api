311f89598ba9f7c22870077eea28ff2f5923c4e5
Dragonfire <bastian616-git@yahoo.de>
2015-11-05 00:14:27 +0100

    Make rc_actionapi.conf a LONGTEXT, otherwise data fits not in this column

--- src/main/java/de/raidcraft/tables/TActionApi.java
+++ src/main/java/de/raidcraft/tables/TActionApi.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 import java.util.Date;
 
@@ -24,6 +25,7 @@ public class TActionApi {
     private String name;
     private String description;
     private String server;
+    @Lob
     private String conf;
     private boolean active;
     private Date lastActive;
