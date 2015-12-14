cdde669b046b36bb9738a03056c9c8afa25e5108
Dragonfire <bastian616-git@yahoo.de>
2015-12-14 22:17:44 +0100

    Remove caching in InventoryManager

--- src/main/java/de/raidcraft/api/inventory/InventoryManager.java
+++ src/main/java/de/raidcraft/api/inventory/InventoryManager.java
@@ -6,9 +6,7 @@ import de.raidcraft.api.Component;
 import org.bukkit.inventory.Inventory;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Philip Urban
@@ -16,51 +14,39 @@ import java.util.Map;
 public class InventoryManager implements Component {
 
     private final RaidCraftPlugin plugin;
-    private Map<Integer, PersistentInventory> loadedInventories = new HashMap<>();
 
     public InventoryManager(RaidCraftPlugin plugin) {
-
         this.plugin = plugin;
         plugin.registerEvents(new InventoryListener());
     }
 
     public PersistentInventory getInventory(int id) throws InvalidInventoryException {
-
-        if (!loadedInventories.containsKey(id)) {
-            if (RaidCraft.getDatabase(RaidCraftPlugin.class).find(TPersistentInventory.class, id) == null) {
-                throw new InvalidInventoryException("Inventory with id " + id + " doesn't exists!");
-            }
-            SQLPersistentInventory inventory = new SQLPersistentInventory(id);
-            loadedInventories.put(inventory.getId(), inventory);
+        if (RaidCraft.getDatabase(RaidCraftPlugin.class).find(TPersistentInventory.class, id) == null) {
+            throw new InvalidInventoryException("Inventory with id " + id + " doesn't exists!");
         }
-        return loadedInventories.get(id);
+        return new SQLPersistentInventory(id);
     }
 
     public PersistentInventory createInventory(Inventory inventory) {
-
         return new SQLPersistentInventory(inventory);
     }
 
     public PersistentInventory createInventory(String title, int size) {
-
         return new SQLPersistentInventory(title, size);
     }
 
+    @Deprecated
     public void loadInventory(PersistentInventory inventory) {
-
-        if (loadedInventories.containsKey(inventory.getId())) {
-            return;
-        }
-        loadedInventories.put(inventory.getId(), inventory);
+        // nothing
     }
 
+    @Deprecated
     public PersistentInventory unloadInventory(PersistentInventory inventory) {
-
-        return loadedInventories.remove(inventory.getId());
+        return inventory;
     }
 
+    @Deprecated
     public List<PersistentInventory> getLoadedInventories() {
-
-        return new ArrayList<>(loadedInventories.values());
+        return new ArrayList<>();
     }
 }
