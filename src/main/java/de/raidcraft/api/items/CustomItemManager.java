6c8bc9d487e82a7999f4a5e5df956124e35adfd8
Michael Reichenbach <michael@mirenoba.com>
2018-09-25 17:53:49 +0200

    feat(items): config id alias Allow items to register config id aliases
    rcitems#1 raidcraft-api#10

--- src/main/java/de/raidcraft/api/items/CustomItemManager.java
+++ src/main/java/de/raidcraft/api/items/CustomItemManager.java
@@ -1,6 +1,7 @@
 package de.raidcraft.api.items;
 
 import com.sk89q.util.StringUtil;
+import de.raidcraft.RaidCraft;
 import de.raidcraft.api.Component;
 import de.raidcraft.util.CaseInsensitiveMap;
 import de.raidcraft.util.CustomItemUtil;
@@ -18,6 +19,8 @@ public final class CustomItemManager implements Component {
     // custom item id | custom item
     private final Map<Integer, CustomItem> customItems = new HashMap<>();
     private final Map<Integer, Gem> loadedGems = new HashMap<>();
+    // item alias | item id
+    private final Map<String, Integer> customItemAliases = Map.of();
     private final Map<String, CustomItem> namedCustomItems = new CaseInsensitiveMap<>();
     // minecraft item id | possible list of custom items
     private final Map<Material, List<CustomItem>> mappedMinecraftItems = new HashMap<>();
@@ -85,10 +88,18 @@ public final class CustomItemManager implements Component {
         try {
             return Optional.ofNullable(getCustomItem(Integer.parseInt(name)));
         } catch (NumberFormatException | CustomItemException ignored) {
-            if (namedCustomItems.containsKey(name)) {
-                return Optional.ofNullable(namedCustomItems.get(name));
+            String lowerCaseName = name.toLowerCase();
+            if (namedCustomItems.containsKey(lowerCaseName)) {
+                return Optional.ofNullable(namedCustomItems.get(lowerCaseName));
+            } else if (customItemAliases.containsKey(lowerCaseName)) {
+                try {
+                    return Optional.ofNullable(getCustomItem(customItemAliases.get(lowerCaseName)));
+                } catch (CustomItemException e) {
+                    RaidCraft.LOGGER.warning("Invalid custom item alias matching: " + name + " -> " + customItemAliases.get(name) + " is not a valid item id.");
+                    return Optional.empty();
+                }
             }
-            return customItems.values().stream().filter(item -> item.getName().equalsIgnoreCase(name)).findFirst();
+            return customItems.values().stream().filter(item -> item.getName().equalsIgnoreCase(lowerCaseName)).findFirst();
         }
     }
 
@@ -98,13 +109,14 @@ public final class CustomItemManager implements Component {
             return getCustomItem(Integer.parseInt(name));
         } catch (NumberFormatException e) {
             // first we need to strip the displayName of any special chars
-            name = ChatColor.stripColor(name);
+            name = ChatColor.stripColor(name).toLowerCase();
             // lets check our named custom items first
-            if (namedCustomItems.containsKey(name)) {
+            if (customItemAliases.containsKey(name)) {
+                return getCustomItem(customItemAliases.get(name));
+            } else if (namedCustomItems.containsKey(name)) {
                 return namedCustomItems.get(name);
             }
             // okay nothing there, so lets search for a matching displayName
-            name = name.toLowerCase();
             List<CustomItem> matching = new ArrayList<>();
             Collection<CustomItem> customItems = new ArrayList<>(this.customItems.values());
             customItems.addAll(namedCustomItems.values());
@@ -150,10 +162,21 @@ public final class CustomItemManager implements Component {
 
     public void registerNamedCustomItem(String name, CustomItem item) throws DuplicateCustomItemException {
 
+        name = name.toLowerCase();
         if (namedCustomItems.containsKey(name)) {
             throw new DuplicateCustomItemException("The custom item with the displayName" + name + " is already registered.");
         }
         namedCustomItems.put(name, item);
+    }
+
+    public void registerCustomItemAlias(int id, String alias) throws DuplicateCustomItemException {
+
+        alias = alias.toLowerCase();
+        if (customItemAliases.containsKey(alias) || namedCustomItems.containsKey(alias)) {
+            throw new DuplicateCustomItemException("A named custom item with the id " + alias + " already exists.");
+        }
+
+        this.customItemAliases.put(alias, id);
     }
 
     public CustomItem unregisterCustomItem(int id) {
