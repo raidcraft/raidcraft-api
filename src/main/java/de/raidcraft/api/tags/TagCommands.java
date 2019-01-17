d72c97b1d97c2dba08de6a9b1712f0e9dcd89bd1
Michael Reichenbach <michael@mirenoba.com>
2019-01-17 20:20:00 +0100

    fix(cmd): make nested commands class static

--- src/main/java/de/raidcraft/api/tags/TagCommands.java
+++ src/main/java/de/raidcraft/api/tags/TagCommands.java
@@ -32,7 +32,7 @@ public class TagCommands {
 
     }
 
-    public class NestedTagCommands {
+    public static class NestedTagCommands {
 
         public NestedTagCommands(RaidCraftPlugin plugin) {
 
